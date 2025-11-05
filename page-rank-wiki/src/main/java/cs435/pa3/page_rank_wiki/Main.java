c720d2934d7396d8cca5f028877f750f33736240
Jacob Heinrich <jr.heinrich09@gmail.com>
2025-11-04 18:56:25 -0700

    Added: - SQL Maven dependency to pom.xml - Text file reading in Main

--- page-rank-wiki/src/main/java/cs435/pa3/page_rank_wiki/Main.java
+++ page-rank-wiki/src/main/java/cs435/pa3/page_rank_wiki/Main.java
@@ -1,7 +1,23 @@
 package cs435.pa3.page_rank_wiki;
 
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String titlePath = args[0];
+        String linkPath = args[1];
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Page Rank Wiki")
+                .master("local[*]")
+                .getOrCreate();
+        if(!titlePath.isEmpty() && !linkPath.isEmpty()) {
+            Dataset<Row> titleData = spark.read().text(titlePath);
+            titleData.show();
+            Dataset<Row> linkData = spark.read().text(linkPath);
+            linkData.show();
+        }
     }
 }
