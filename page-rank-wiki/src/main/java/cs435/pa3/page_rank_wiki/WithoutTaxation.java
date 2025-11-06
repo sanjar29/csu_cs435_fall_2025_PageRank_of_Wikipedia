5379ae46ec6bb190df978511888ffa504fb7d07c
Jacob Heinrich <jr.heinrich09@gmail.com>
2025-11-06 15:21:10 -0700

    Fixed bug in Page Rank Calculation

--- page-rank-wiki/src/main/java/cs435/pa3/page_rank_wiki/WithoutTaxation.java
+++ page-rank-wiki/src/main/java/cs435/pa3/page_rank_wiki/WithoutTaxation.java
@@ -20,32 +20,25 @@ public class WithoutTaxation {
      * @return scala.Tuple2 containing page Id and page data including page rank
      */
     public static JavaPairRDD<Long, scala.Tuple3<String, Double, String>> getPageRank(JavaPairRDD<Long, scala.Tuple3<String, Double, String>> inData, Double inCount) {
-        // Types declared here for readability
-        JavaPairRDD<Long, Double> calculatePageRank;
-        JavaPairRDD<Long, scala.Tuple3<String, Double, String>> data;
-        ArrayList<scala.Tuple2<Long, Double>> adjacentPageRank = new ArrayList<>();
-        JavaPairRDD<Long, Double> adjacentPageRanks;
-        JavaPairRDD<Long, scala.Tuple2<Double, scala.Tuple3<String, Double, String>>> updatedPageRank;
-        JavaPairRDD<Long, scala.Tuple3<String, Double, String>> returnPageRank;
-
-        data = inData;
+        JavaPairRDD<Long, scala.Tuple3<String, Double, String>> data = inData;
         Double count = inCount;
         // Propagates page ranking to adjacent pages
-        calculatePageRank = data.flatMapToPair(link -> {
+        JavaPairRDD<Long, Double> calculatePageRank = data.flatMapToPair(link -> {
             String adjacentPages = link._2._3().trim();
             String[] pagesArray = adjacentPages.split("\\s+");
             int numberOfPages = pagesArray.length;
             Double pageRank = link._2._2();
+            ArrayList<scala.Tuple2<Long, Double>> adjacentPageRank = new ArrayList<>();
             for (int i = 0; i < numberOfPages; i++) {
                 // First tuple value is destination page ID and second is updated page rank of current page
                 adjacentPageRank.add(new scala.Tuple2(Long.valueOf(pagesArray[i]), pageRank * (1D/Double.valueOf(numberOfPages))));
             }
             return adjacentPageRank.iterator();
         });
-        adjacentPageRanks = calculatePageRank.reduceByKey((value1, value2) -> value1 + value2);
-        updatedPageRank = adjacentPageRanks.join(data);
+        JavaPairRDD<Long, Double> adjacentPageRanks = calculatePageRank.reduceByKey((value1, value2) -> value1 + value2);
+        JavaPairRDD<Long, scala.Tuple2<Double, scala.Tuple3<String, Double, String>>> updatedPageRank = adjacentPageRanks.join(data);
         // Final map to return new page rank data for all pages
-        returnPageRank = updatedPageRank.mapToPair(page -> {
+        JavaPairRDD<Long, scala.Tuple3<String, Double, String>> returnPageRank = updatedPageRank.mapToPair(page -> {
             Double newPageRank = page._2._1();
             return new scala.Tuple2<>(page._1(), new scala.Tuple3<>(page._2._2._1(), newPageRank,page._2._2._3()));
         });
